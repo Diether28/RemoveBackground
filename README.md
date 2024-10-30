faa6b76c67683ca74cbb4761326145e75440fd4e
Diether28 <72750459+Diether28@users.noreply.github.com>
2024-10-30 01:38:15 -0600

    precision y mejoras

--- README.md
+++
@@ -1,4 +0,0 @@
-"# Remove-Background" 
-"# Remove-Background" 
-"# Remove-Background" 
-"# RemoveBackground" 
--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 #importamos los framework a utilizar
-from cProfile import label
-
 import streamlit as st
 from rembg import remove
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 
-# from streamlit import download_button
-
-st.set_page_config(layout="wide", page_title="Eliminador de fondos de imagenes")
-
-st.write("## Elimina el fondo de tu imagen")
+# Configuracion de la pagina
+st.set_page_config(
+    layout="wide", 
+    page_title="Eliminador de fondos de imagenes"
+)
+st.title(" 🖼️Elimina el fondo de tu imagen")
 
 st.write(
     ":dog: Sube una imagen y observa cómo se elimina mágicamente el fondo. "
@@ -19,33 +18,68 @@ st.write(
     "¡Gracias especiales a la biblioteca rembg! :grin:"
 )
 
+# seleccion de subidas y opciones de descarga en la barra lateral
 st.sidebar.write("## Subir y descargar :gear:")
 
+# Tamaño maximo permitido
 MAX_TAMAÑO_ARCHIVO = 5 * 1024 * 1024 #5MB
 
+# Opciones para formato y nombre del archivo
+formato = st.sidebar.selectbox(
+    "Selecciona el formato de descarga",["PNG","JPEG"]
+)
+nombre_image = st.sidebar.text_input(
+    "Ingrese el nombre del archivo",
+    value="imagen_sin_fondo"
+)
+
 def arreglar_imagen(imagen_subida):
-    imagen = Image.open(imagen_subida)
+    try:
+        # Intentar abrir con el soporte de transparencia
+        imagen = Image.open(imagen_subida).convert("RGBA")
+    except UnidentifiedImageError:
+        st.error("Error: No se pudo abrir la imagen. Asegúrate de subir un archivo válido.")
+        return
+    # Mostrar la columna para comparación
     col1, col2 = st.columns(2)
-
     col1.write("Imagen original :camera:")
-    col1.image(imagen)
+    col1.image(imagen, use_column_width=True)
 
-    sin_fondo = remove(imagen)
-    col2.write("Imagen sin fondo :winking_face:")
-    col2.image(sin_fondo)
+    # Remover fondo
+    try:
+        sin_fondo = remove(imagen)
+    except Exception as e:
+        st.error(f"Hubo un error al procesar la imagen: {str(e)}")
+        return
+    
+    col2.write("Imagen sin fondo ✨")
+    col2.image(sin_fondo, use_column_width=True)
 
 #     preparar la imagen para la descarga
     buffered = BytesIO()
-    sin_fondo.convert("RGB").save(buffered, format("JPEG"))
-    buffered.seek(0)
+    if formato == "JPEG":
+        sin_fondo = sin_fondo.convert("RGB") 
+        # Comvertir para compatibilidad con JPEG
+        sin_fondo.save(
+            buffered,
+            format="JPEG",
+            quality=90
+        )
+        mime_type = "image/png"
+        extension = "png"
 
+        buffered.seek(0)
+
+        # Boton de descarga
     st.download_button(
         label="Descaragar Imagen Sin Fondo",
         data=buffered,
-        file_name="Imagen_sin_fondo.jpg",
-        mime="image/jpeg"
+        file_name=f"{nombre_image}.{extension}",
+        mime_type="image/jpeg"
     )
-mi_subida = st.sidebar.file_uploader("Sube una imagen", type=["png","jpg","jpeg"])
+
+    # Subida de imagen en la barra lateral
+mi_subida = st.sidebar.file_uploader("Sube una imagen (JPG,PNG,JPEG)", type=["png","jpg","jpeg"])
 
 if mi_subida is not None:
     if mi_subida.size > MAX_TAMAÑO_ARCHIVO:
